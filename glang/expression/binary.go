57e982a23b46abc8adc1ceec3dbb3d86425a1616
almeng <almeng1234556@gmail.com>
2021-11-15 14:11:18 +0900

    project structure changed

---
+++ glang/expression/binary.go
@@ -0,0 +1,30 @@
+package expression
+
+import (
+	"almeng.com/glang/glang/syntax"
+	"almeng.com/glang/glang/token"
+)
+
+type BinaryExpressionSyntax struct {
+	OperatorToken SyntaxToken
+	Left          syntax.ExpressionSyntax
+	Right         syntax.ExpressionSyntax
+}
+
+func (b BinaryExpressionSyntax) Kind() token.Token {
+	return b.OperatorToken.Token
+}
+func (b BinaryExpressionSyntax) Type() syntax.Type {
+	return syntax.ExpBi
+}
+func (b BinaryExpressionSyntax) GetChildren() []syntax.ExpressionSyntax {
+	return []syntax.ExpressionSyntax{b.Left, b.OperatorToken, b.Right}
+}
+
+func NewBinaryExpressionSyntax(left syntax.ExpressionSyntax, opToken SyntaxToken, right syntax.ExpressionSyntax) *BinaryExpressionSyntax {
+	bi := &BinaryExpressionSyntax{
+		opToken,
+		left,
+		right}
+	return bi
+}
